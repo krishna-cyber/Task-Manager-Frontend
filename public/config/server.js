22f272860455013fae4ecc01ed22294d8b5f6861
krishna tiwari <tiwarikrishna54321@gmail.com>
2023-03-04 19:09:31 +0545

    server timeout changed

--- public/config/server.js
+++ public/config/server.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const server = axios.create({
   baseURL: "https://backend-task-manager-api.onrender.com",
-  timeout: 1000,
+  timeout: 3000,
 });
 
 export default server;
